0abb2bee7e06e41e8c40bb62d0899cf76e9ce6d0
ttakasawa <ttakasawa0000@gmail.com>
2024-12-22 22:47:21 -0800

    fix dock

--- application/app.py
+++ application/app.py
@@ -157,5 +157,5 @@ logger.add(log_to_elasticsearch, serialize=lambda record: json.dumps(record))
 
 
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', debug=True, threaded=True, port=5000)
+    flask_app.run(host='0.0.0.0', debug=True, threaded=True, port=8080)
 
